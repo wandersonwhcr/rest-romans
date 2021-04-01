061f6fe3c60e70f544c83c92e068b80e0be7a9ee
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-04-01 17:13:49 -0300

    Initialize Slim

--- public/index.php
+++ public/index.php
@@ -1,1 +1,11 @@
-<?php phpinfo();
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->addErrorMiddleware(true, true, true);
+
+$app->run();
