712edd466a68425f21b874bcceb2934c0798994d
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-04-02 07:55:27 -0300

    Create a Home action to response default requests

--- app.php
+++ app.php
@@ -2,10 +2,13 @@
 
 declare(strict_types=1);
 
+use Rest\Romans\Action;
 use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
 
 $app->addErrorMiddleware(true, true, true);
+
+$app->get('/', Action\Home::class);
 
 return $app;
