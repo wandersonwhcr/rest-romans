7af1ec3abd28b3f6d2ef6fd73428fb63238bc2bd
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-04-03 07:34:35 -0300

    Use correct filter

--- src/Action/Arabics.php
+++ src/Action/Arabics.php
@@ -6,9 +6,12 @@ namespace Rest\Romans\Action;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface as RequestInterface;
+use Romans\Filter\IntToRoman as IntToRomanFilter;
 
 class Arabics
 {
+    private ?IntToRomanFilter $intToRomanFilter;
+
     /**
      * @param array<string,string> $args
      */
@@ -16,9 +19,17 @@ class Arabics
     {
         unset($request, $args);
 
-        $response->getBody()->write('{"arabic":"1999","roman":"MCMXCIX"}');
+        $response->getBody()->write(json_encode([
+            'arabic' => '1999',
+            'roman'  => $this->getIntToRomanFilter()->filter(1999),
+        ]));
 
         return $response->withStatus(200)
             ->withHeader('Content-Type', 'application/json');
+    }
+
+    private function getIntToRomanFilter(): IntToRomanFilter
+    {
+        return $this->intToRomanFilter ?? new IntToRomanFilter();
     }
 }
